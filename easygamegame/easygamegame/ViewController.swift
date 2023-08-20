1dabf0f87a6825e4d5cf1bf48b41a57948a0ce77
Ground <27danny@naver.com>
2023-08-20 22:38:16 +0900

    대략적인 UI 그리기

--- easygamegame/easygamegame/ViewController.swift
+++ easygamegame/easygamegame/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userBallCountLbl: UILabel!
+    @IBOutlet weak var computerBallCountLbl: UILabel!
+    
+    var comBallsCount: Int = 20
+    var userBallsCount: Int = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        computerBallCountLbl.text = String(comBallsCount)
+        userBallCountLbl.text = String(userBallsCount)
     }
 
-
+    @IBAction func gameStartPressed(_ sender: Any) {
+        print("게임시작!")
+    }
+    
 }
 
